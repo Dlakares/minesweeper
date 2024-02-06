03cdd9dd2da3ab78a61484d5732c24cefb2aae39
Rustam <sombra1nf@yandex.ru>
2024-02-06 17:19:04 +0500

    Completed tests

--- src/test/java/ru/studiotg/minesweeper/util/FieldBuilderTest.java
+++ src/test/java/ru/studiotg/minesweeper/util/FieldBuilderTest.java
@@ -3,12 +3,13 @@ package ru.studiotg.minesweeper.util;
 import org.junit.jupiter.api.Test;
 import ru.studiotg.minesweeper.entity.Field;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FieldBuilderTest {
 
-    FieldBuilder builder = new FieldBuilder(' ','X', 'c');
+    FieldBuilder builder = new FieldBuilder(' ', 'X', 'c');
 
     @Test
     void getGameField() {
@@ -17,6 +18,7 @@ class FieldBuilderTest {
             assertEquals(3, field.getWidth());
             assertEquals(3, field.getHeight());
             assertEquals(1, field.getMinesCount());
+            assertTrue(Arrays.stream(field.getField()).anyMatch(s -> s.contains("X")));
         });
     }
 }
